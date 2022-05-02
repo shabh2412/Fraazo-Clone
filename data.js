399e04708530db236b9d89bbf47966ebb839c38e
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-05-03 00:10:00 +0530

    Added fruits data

---
+++ data.js
@@ -0,0 +1,134 @@
+var data = [
+    {
+        "name": "Mango Banganapalli",
+        "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/images/images/000/003/372/original/data?1643986229",
+        "quantity": "1kg",
+        "price": "99"
+    },
+    {
+        "name": "Bottle Gourd (Dudhi / Lauki)",
+        "image": "https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VBOT12.png",
+        "quantity": "1 pc",
+        "price": "11"
+    },
+    {
+        "name": "Beans Cluster (Gavar)",
+        "image": "https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VGAV12.png",
+        "quantity": "250 g",
+        "price": "9"
+    },
+    {
+        "name": "Ash Gourd (White Pumpkin)",
+        "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/164/original/data?1629975839",
+        "quantity": "500 g",
+        "price": "9"
+    },
+    {
+        "name": "Tondli",
+        "image": "https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VTON12.png",
+        "quantity": "500 g",
+        "price": "8"
+    },
+    {
+        "name": "Pumpkin Green",
+        "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/101/original/data?1612451098",
+        "quantity": "500 g",
+        "price": "7"
+    },
+    {
+        "name": "Lady's Finger (Bhindi / Okra / Bhendi)",
+        "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/images/images/000/006/624/original/data?1649243792",
+        "quantity": "500 g",
+        "price": "13"
+    },
+    {
+        "name": "Lemon(100g) / Nimboo / Nibu",
+        "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/003/250/original/data?1642167498",
+        "quantity": "100g",
+        "price": "21"
+    },
+    {
+        "name": "Onion",
+        "image": "https://fraazo-master.s3.ap-south-1.amazonaws.com/products/BONI11.png",
+        "quantity": "1 kg",
+        "price": "16"
+    },
+    {
+        "name": "Brinjal Bharta",
+        "image": "https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VBRI12.png",
+        "quantity": "500 g",
+        "price": "14"
+    },
+    {
+        "name": "Papaya",
+        "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/092/original/data?1620114635",
+        "quantity": "1 pc",
+        "price": "29"
+    },
+    {
+        "name": "Watermelon",
+        "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/117/original/data?1616765390",
+        "quantity": "1 pc",
+        "price": "29"
+    },
+    {
+        "name": "Pineapple",
+        "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/157/original/data?1595448197",
+        "quantity": "800g -1 Kg",
+        "price": "55"
+    },
+    {
+        "name": "Musk Melon/ Kharabooja",
+        "image": "https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FMUS1.png",
+        "quantity": "1 pc",
+        "price": "29"
+    },
+    {
+        "name": "Capsicum (Shimla), Green Shimla Mirchi",
+        "image": "https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VCAP12.png",
+        "quantity": "250 g",
+        "price": "20"
+    },
+    {
+        "name": "Potato / Aloo",
+        "image": "https://fraazo-master.s3.ap-south-1.amazonaws.com/products/BPOT11.png",
+        "quantity": "1 kg",
+        "price": "29"
+    },
+    {
+        "name": "Tomato (1 Kg)",
+        "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/images/images/000/006/605/original/data?1649242645",
+        "quantity": "1 kg",
+        "price": "45"
+    },
+    {
+        "name": "Mango Lalbhag Sindhura",
+        "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/250/original/data?1621065378",
+        "quantity": "1 Kg",
+        "price": "96"
+    },
+    {
+        "name": "Kiwi Green",
+        "image": "https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FKIW2.png",
+        "quantity": "1 unit",
+        "price": "69"
+    },
+    {
+        "name": "Pear Green / Packham",
+        "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/096/original/data?1625415915",
+        "quantity": "500 g",
+        "price": "99"
+    },
+    {
+        "name": "Dry Dates - 200gms",
+        "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/596/original/data?1635146122",
+        "quantity": "200 g",
+        "price": "70"
+    },
+    {
+        "name": "Drumstick",
+        "image": "https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VDRU12.png",
+        "quantity": "100 g",
+        "price": "3"
+    }
+]
