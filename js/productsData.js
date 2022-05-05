0b98a3810f13e89de03fa2a639c07cd93e175120
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-05-05 18:53:49 +0530

    started working on listing page

---
+++ js/productsData.js
@@ -0,0 +1,14 @@
+prod = {
+    "id": 0,
+    "name": "Rishabh Panesar",
+    "image": "https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/329/original/data?1616795305",
+    "qty": "4",
+    "metric": "pcs",
+    "currentPrice": "200",
+    "benefits": "- Alphanso mango also very healthy highly contains vitamins like Vitamin E, Vitamin A, Carotene and minerals like Magnesium, Manganese, Phosphorus. - Did you know it helps to regulating cholestrol levels in body and digestion process by adding mango in your diet during summer season. It can be used in dried form as well and used as part of salads. - It is also season best fruit to increases eye sight, prevents night blindness and dry eyes health issues.\n",
+    "info": "- Alphanso mango also very healthy highly contains vitamins like Vitamin E, Vitamin A, Carotene and minerals like Magnesium, Manganese, Phosphorus. - Did you know it helps to regulating cholestrol levels in body and digestion process by adding mango in your diet during summer season. It can be used in dried form as well and used as part of salads. - It is also season best fruit to increases eye sight, prevents night blindness and dry eyes health issues.\n",
+    "oldPrice": "150"
+}
+
+arr = [prod,prod,prod,prod,prod,prod,prod];
+console.log(arr);
